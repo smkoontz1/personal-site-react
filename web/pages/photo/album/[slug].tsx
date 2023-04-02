8715db58f9254d62ccc6059375753144667d3162
smkoontz1 <smkoontz1@gmail.com>
2023-04-01 19:02:45 -0500

    Replaced react image gallery with react photo album

--- web/pages/photo/album/[slug].tsx
+++ web/pages/photo/album/[slug].tsx
@@ -4,10 +4,10 @@ import { ReactElement } from 'react'
 import MainLayout from '../../../components/common/MainLayout'
 import SanityClient from '../../../sanityClient'
 import { NextPageWithLayout } from '../../_app'
-import ImageGallery, { ReactImageGalleryItem } from 'react-image-gallery'
 import { urlFor } from '../../../utilities/sanityUtils'
 import styles from '../../../styles/photo/album/Album.module.scss'
 import AlbumLayout from '../../../components/photography/album/AlbumLayout'
+import { PhotoAlbum, Photo } from 'react-photo-album'
 
 interface Props {
   album: AlbumResponse
@@ -18,28 +18,49 @@ interface AlbumResponse {
   images: SanityImageSource[]
 }
 
+interface SanityImageSourceRef {
+  asset: {
+    _ref: string
+  }
+}
+
+const SANITY_DIMENSIONS_REGEX = /-([0-9]+)x([0-9]+)-[a-z]+$/
+
+const getDimensions = (sanityImgRef: string): [number, number] => {
+  let width = 0
+  let height = 0
+  
+  const match = sanityImgRef.match(SANITY_DIMENSIONS_REGEX)
+  if (match && match.length >= 3) {
+    width = Number.parseInt(match[1])
+    height = Number.parseInt(match[2])
+  }
+
+  return [width, height]
+} 
+
 const Album: NextPageWithLayout<Props> = ({ album }) => {
   // TODO: destructure the props
   // For some reason it breaks the build
-  
-  const imageGalleryItems: ReactImageGalleryItem[] = album?.images.map(img => {
+
+  const sanityImages = album?.images as (SanityImageSource & SanityImageSourceRef)[]
+
+  const photoAlbumImages: Photo[] = sanityImages.map(img => {
+    const imgRef = img.asset._ref
+    const dimensions = getDimensions(imgRef)
+
     return {
-      original: urlFor(img).url(),
-      thumbnail: urlFor(img).width(200).url(),
+      src: urlFor(img).url(),
+      width: dimensions[0],
+      height: dimensions[1]
     }
-  }) ?? []
+  })
 
   return (
     <>
       <h1>{album?.title.toUpperCase()}</h1>
       <div className={styles.galleryComponent}>
-        <ImageGallery
-          items={imageGalleryItems}
-          lazyLoad
-          thumbnailPosition='right'
-          slideDuration={1000}
-          slideInterval={5000}
-        />
+        <PhotoAlbum photos={photoAlbumImages} layout={'rows'} />
       </div>
     </>
   )
